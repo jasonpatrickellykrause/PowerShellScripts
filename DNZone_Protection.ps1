582706a9821890fad86c94fd152c662f59f237e8
Jason Krause <8408603+jasonpatrickellykrause@users.noreply.github.com>
2021-08-14 09:24:47 -0600

    Add files via upload

---
+++ DNZone_Protection.ps1
@@ -0,0 +1,22 @@
+﻿Import-Module ActiveDirectory
+
+# Get all domains in forest
+$Domains = (Get-ADForest).Domains
+
+foreach ($Domain in $Domains)
+{
+    # Get all DCs in Domain
+    $DCs = Get-ADDomainController -filter * -Server $Domain
+
+    foreach ($DC in $DCs)
+    {
+        $DomainController_fqdn = $DC.HostName
+        $DomainDN = $dc.DefaultPartition
+        Get-ADobject -Server $DomainController_fqdn -Filter {objectclass -eq "DNSZone"} | 
+            Set-ADObject -ProtectedFromAccidentalDeletion $true
+        Get-ADobject -Server $DomainController_fqdn -Filter {objectclass -eq "DNSZone"} -SearchBase "DC=DomainDNSZones,$DomainDN" | 
+            Set-ADObject -ProtectedFromAccidentalDeletion $true
+        Get-ADobject -Server $DomainController_fqdn -Filter {objectclass -eq "DNSZone"} -SearchBase "DC=ForestDNSZones,$DomainDN" | 
+            Set-ADObject -ProtectedFromAccidentalDeletion $true
+    }
+} 
---
+++ ie version.ps1
@@ -0,0 +1,4 @@
+﻿Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Internet Explorer\' -Name svcVersion
+
+(Get-Item 'C:\Program Files\internet explorer\iexplore.exe').VersionInfo | FL
+(Get-Item 'C:\Program Files (x86)\internet explorer\iexplore.exe').VersionInfo | FL
